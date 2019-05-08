93d223141af7c655c1a24064a05afa98cfceac43
hubhack <2329259856@qq.com>
2019-05-08 19:24:00 +0800

    add

--- ls.py
+++ ls.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import argparse
+# 获得一个参数解析器
+parser = argparse.ArgumentParser(prog='ls', add_help=True, description='list directory contents')
+parser.add_argument('-l', action='store_true', help='use a long listing format')
+parser.add_argument('-a', '--all', action='store_true', help='show all files, do not ignore')
 
-parser = argparse.ArgumentParser()
-args = parser.parse_args()
+
+args = parser.parse_args() # 分析参数, 同时传入可迭代的参数
+# parser.print_help()
+print(args) # 打印名词空间中收集的参数
 parser.print_help()
+# 运行结果
+
+
