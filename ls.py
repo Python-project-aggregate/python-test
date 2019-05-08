35fdaadbd72f208ce2498f257ccd98646d6c2203
hubhack <2329259856@qq.com>
2019-05-08 21:10:47 +0800

    初步

--- ls.py
+++ ls.py
@@ -6,4 +6,4 @@ from datetime import datetime
 import stat
 # 获得一个参数解析器
 parser = argparse.ArgumentParser(prog = 'ls', add_help=False, description='list directory')
-parser
+parser.add_argument('path', nargs='?', default='.', help = "directory")
