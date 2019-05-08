b08b8a21856fbed9d0ad7b831bf763be1f34c396
hubhack <2329259856@qq.com>
2019-05-08 21:08:17 +0800

    初步

--- ls.py
+++ ls.py
@@ -3,9 +3,6 @@
 import argparse
 from pathlib import Path
 from datetime import datetime
+import stat
 # 获得一个参数解析器
-parser = argparse.ArgumentParser(prog='ls', add_help=True, description='list directory contents')
-parser.add_argument('path', nargs='?',default='.',help='directory')
-parser.add_argument('-l', action='store_true', help='use a long listing format')
-parser.add_argument('-a', '--all', action='store_true', help='show all files, do not ignore')
-
+parser = argparse.ArgumentParser(prog = 'ls')
