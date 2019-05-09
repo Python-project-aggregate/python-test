f393313ee9d5677220d6cf7f3663fc3c4d6c1b47
hubhack <2329259856@qq.com>
2019-05-09 20:18:35 +0800

    add

--- files.py
+++ files.py
@@ -3,3 +3,6 @@ d = {}
 with open(filename, encoding='utf-8') as f:
     for line in f:
         words = line.split()
+        for word in map(str.lower, word):
+            d[word] = d.get(word, 0) + 1
+print(sorted(d.items(), key = lambda item:item[1], reverse=True))
