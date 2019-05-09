316501e03be7d3334065e28524ddb50c601d0619
hubhack <2329259856@qq.com>
2019-05-09 20:19:57 +0800

    add

--- files.py
+++ files.py
@@ -6,3 +6,6 @@ with open(filename, encoding='utf-8') as f:
         for word in map(str.lower, word):
             d[word] = d.get(word, 0) + 1
 print(sorted(d.items(), key = lambda item:item[1], reverse=True))
+from collections import defaultdict
+d = defaultdict(lambda :0)
+with open(filename, encoding='utf-80') as f:
