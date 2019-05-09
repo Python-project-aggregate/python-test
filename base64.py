80440ed9d318df914b56f13472ad0ec91a14fe60
hubhack <2329259856@qq.com>
2019-05-09 20:46:38 +0800

    add

--- base64.py
+++ base64.py
@@ -2,4 +2,9 @@ alphabet = b'ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz0123456789+/'
 print(alphabet)
 src = 'abcd'
 def base64encode(src:str):
-    ret = bytearray
+    ret = bytearray()
+    if isinstance(src, str):
+        _src = src.encode()
+    else:
+        return
+    length = len(_src)
