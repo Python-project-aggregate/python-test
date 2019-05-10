24e511d17efb7e8fddc91c46a2a482266ce73c3b
hubhack <2329259856@qq.com>
2019-05-10 12:23:35 +0800

    test2 更新

--- test2/substrings.py
+++ test2/substrings.py
@@ -8,22 +8,21 @@ def substrings():
     lst1 = len(a)
     lst2 = len(b)
     max = 0 # 最大值,默认为0
-    lst = []
-    record = { j:[i for i in a] for j in b}
-    print(record)
+    p = 0
+    record = [[0 for i in range(lst1+1)] for j in range(lst2+1)]
+    # print(record)
     for i in range(lst1):
         for j in range(lst2):
-            record[0][i +1] = a[i]
+            record[0][i+1] = a[i]
             record[j+1][0] = b[j]
             # record[0][0] = 0
             if a[i] == b[j]:
-                m = 1
-                if m > max:
-                    record[j+1][i+1] = m
-                    m += 1
-                    lst.append(a[i])
-
-
+                record[j+1][i+1] = record[j+1][i+1] + 1
+                if record[j+1][i+1] > max:
+                    max = record[j+1][i+1]
+                    p = i + 1
+    # print(*record, sep='\n')
+    print(a[p-max:p])
     # lst.append(*record)
 
 
--- test2/test.py
+++ test2/test.py
@@ -1,5 +1,19 @@
 a = 'adsf'
 b ='adsfde'
 
-record = {j: [i for i in a] for j in b}
-print(record)
+source = {j: {i:1 for i in a} for j in b}
+# print(record)
+# target = {}
+def flatmap(src, dest=None,prefix=''):
+    if dest == None:
+        dest = {}
+
+
+    for k, v in src.items():
+        if isinstance(v, dict):
+            flatmap(v, dest, prefix = prefix+ k +'.')
+        else:
+            dest[prefix+k] =v
+    # _faltmap(src)
+    return dest
+print(flatmap(source))
