731f8d3319d2e1ed08a7e985b4d476ec60bd5e1c
hubhack <2329259856@qq.com>
2019-05-11 09:10:16 +0800

    test1 更新

--- test3/code/clock.py
+++ test3/code/clock.py
@@ -1,8 +1,18 @@
-'''
-定义使用时钟类
-'''
-class Student(object):
-    def __init__(self,name, age):
-        self.name = name
-        self.age = age
-    def study(self, ):
+
+import timeit
+def test1(strlist):
+    return "".join(strlist)
+
+def test2(strlist):
+    result = ""
+    for v in strlist:
+        result = result+v
+    return result
+
+if __name__ == "__main__":
+    strlist = ["a very very very very very very very long string" for n in range(100000)]
+    timer1 = timeit.Timer("test1(strlist)", "from __main__ import strlist, test1")
+    timer2 = timeit.Timer("test2(strlist)", "from __main__ import strlist, test2")
+    time1 = timer1.timeit(number=100)
+    time2 = timer2.timeit(number=100)
+    print("join: %f, plus: %f" % (time1, time2))
