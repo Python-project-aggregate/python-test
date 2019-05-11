407b2b713708b4b27cf90d7f2017b06530ad4ee7
hubhack <2329259856@qq.com>
2019-05-11 10:29:04 +0800

    test4 更新

--- test3/code/b.py
+++ test3/code/b.py
@@ -1,6 +1,10 @@
-from a import PI
-import a
-def main():
-    return PI * 2
-print(main())
-print(a.__name__)
+def foo1():
+    msg = 'zen of python'
+    def foo2():
+        print(msg)
+    return foo2
+a = foo1()
+def fun():
+    print(5)
+print(fun.__closure__)
+print(a.__closure__[1].cell_contents)
