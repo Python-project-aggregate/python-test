387f74d754931e83d1f74f02affe4364971ce93b
hubhack <2329259856@qq.com>
2019-05-12 15:30:01 +0800

    add

--- test5/_re.py
+++ test5/_re.py
@@ -1,11 +1,19 @@
 import queue
+import threading
+import time
 
+a = [1, 2, 3]
 q = queue.Queue()
 q.put(1)
 q.put('abc')
+# q.put(a)
+print(q.get())
 print(q.get())
 # print(q.get())
-# print(q.get(True, 5))# 同步阻塞 True 是等 ,成功拿到数据不会抛异常
-print(q.get_nowait())# 同步阻塞
-# 多线程可以打开,
-print(q.get(True))
+def handle(q:queue.Queue):
+    time.sleep(5)
+    q.put('xyz')
+t = threading.Thread(target =handle,args = (q,))
+t. start() # 启动
+print(q.get()) # empty
+
