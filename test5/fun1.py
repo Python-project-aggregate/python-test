390f2f54a66650344b5aa48fe9e78754b03f45fc
hubhack <2329259856@qq.com>
2019-05-13 20:32:18 +0800

    add

--- test5/fun1.py
+++ test5/fun1.py
@@ -10,3 +10,6 @@ def print_tree(array, unit_width=2):
     length = len(array)
     index = 1
     depth = math.ceil(math.log2(length))
+    space = ' '* unit_width
+    for i in range(depth-1, -1, -1):
+        pre = 2** i - 1
