10eafeac385f3228446363d07410a454c73dea03
hubhack <2329259856@qq.com>
2019-05-13 20:34:26 +0800

    add

--- test5/fun1.py
+++ test5/fun1.py
@@ -16,3 +16,4 @@ def print_tree(array, unit_width=2):
         print(pre * space, end = '')
         offset = 2 ** (depth - 1 -1 )
         line = array[index:index + offset]
+        interval = (2 * pre + 1) * space
