d7c85de7add43ffbc290fe854c33a3e35707d703
hubhack <2329259856@qq.com>
2019-05-14 17:51:04 +0800

    add

--- test6/code/module1.py
+++ test6/code/module1.py
@@ -7,3 +7,24 @@ regex = re.compile(pattern)
 conversion = {
     'datetime': lambda timestr: datetime.datetime.strptime(timestr,%d/%b/%Y:%H:%M:%S%z), 'status': int, 'length':int
 }
+def extract(logline:str) -> dict:
+    m = regex.match(logline)
+    if m:
+        return {k:conversion.get(k, lambda x:x)(v) for k,v in m.groudict().items()}
+    else:
+        return None
+def loadfile(filename:str, encoing='utf-8'):
+    with open(filename, encoding=encoing) as f:
+        for line in f:
+            fields = extract(line)
+            if fields:
+                yield fields
+            else:
+                continue
+
+def load(*path, encoding='utf-8', ext='*.log', recursive=False):
+    for x in paths:
+        print(x)
+        p = Path(x)
+        if p.is_dir():
+            if isinstance(ext, str):
