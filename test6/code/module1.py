df47868b3c953e7fe0bdbfa982674b3a3b6c3008
hubhack <2329259856@qq.com>
2019-05-14 17:56:56 +0800

    add

--- test6/code/module1.py
+++ test6/code/module1.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import datetime
 import re
 pattern = '''(?p<remote>[\d.]{7,} - - \[(?p<datetime>[\w/: +]+\] \ 
-"(?p<method>\w+) (?p<url>\s+) (?p<protocol[\w\d/.]+>)")'''
+"(?p<method>\w+) (?p<url>\s+) (?p<protocol[\w\d/.]+>)") (?p<status>\d+)'''
 regex = re.compile(pattern)
 conversion = {
     'datetime': lambda timestr: datetime.datetime.strptime(timestr,%d/%b/%Y:%H:%M:%S%z), 'status': int, 'length':int
