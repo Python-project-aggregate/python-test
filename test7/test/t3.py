f28ed1cecdba73ec9657b00a26b9194018a99629
hubhack <2329259856@qq.com>
2019-05-16 15:34:59 +0800

    add

--- test7/test/t3.py
+++ test7/test/t3.py
@@ -1,6 +1,7 @@
 # 车辆信息
 class Car:
     _num = 0
+
     def __init__(self, mark='vov', color=0, price=0, speed=0):
         self.mark = mark
         self.color = color
@@ -10,6 +11,9 @@ class Car:
     def add_car(self):
         d = {}
         d[self.mark] = '{}-{}-{}'.format(self.color, self.price, self.speed)
-        print(d,)
-a = Car('1',0,0,0)
-a.add_car()
+        print(d, )
+
+if __name__ == '__main__':
+
+    a = Car('1', 0, 0, 0)
+    a.add_car()
--- test7/test/t5.py
+++ test7/test/t5.py
@@ -1,1 +1,14 @@
 # 模拟购物车购物
+
+class Person:
+    def __init__(self, name, age=18):
+        self.name = name
+        self.__age = age
+    def age(self):
+        return self.__age
+    def set_age(self,age):
+        self.__age = age
+
+tom = Person('tom')
+print(tom.age())
+tom.set_age(20)
