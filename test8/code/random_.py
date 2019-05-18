4b45117547545d242912c14146f569e652f6d69d
hubhack <2329259856@qq.com>
2019-05-18 16:03:30 +0800

    add

---
+++ test8/code/random_.py
@@ -0,0 +1,16 @@
+
+import random
+class RandomGen:
+    def __init__(self, start=1, stop = 100, count = 10):
+        self.start = start
+        self.stop = stop
+        self.count = count
+        self._gen = self._generate()
+
+    def _generate(self):
+        while True:
+            yield random.randint(self.start, self.stop)
+
+    def generate(self):
+        yield from (next(self._gen) for i in range(self.count))
+print(list(RandomGen().generate()))
