0e88b1992440b396f5ef8a3b40083032e503bd71
hubhack <2329259856@qq.com>
2019-05-19 00:49:58 +0800

    add

--- test8/code/fun2.py
+++ test8/code/fun2.py
@@ -1,15 +1,3 @@
-import random
-class RandomGen:
-    def __init__(self, start = 1, stop = 100, count = 10):
-        self.start = start
-        self.stop = stop
-        self.count = count
-        self._gen = self._generate()
-    def _generate(self):
-        while True:
-            yield random.randint(self.start , self.stop)
-
-    def generate(self):
-        return [next(self._gen) for i in range(self.count)]
-
-print(RandomGen().generate())
+'''
+列表， 遍历 ，
+'''
