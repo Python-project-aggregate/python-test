26467462194b325b3e7c048fcca471ed9a74d097
hubhack <2329259856@qq.com>
2019-05-23 12:19:10 +0800

    add

--- test9/code/test1.py
+++
@@ -1,9 +0,0 @@
-class PrintableMixin:
-    def print(self):
-        print(self.content, 'mixin')
-
-class SuperPrintableMixin(PrintableMixin):
-    def print(self):
-        print('`'*20)
-        super().print()
-        print('---'*20)
