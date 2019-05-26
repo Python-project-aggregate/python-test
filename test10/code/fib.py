6ad7e4da829c25ec6f095eeb8703fafeb96d9ecc
hubhack <2329259856@qq.com>
2019-05-26 10:33:06 +0800

    add

--- test10/code/fib.py
+++ test10/code/fib.py
@@ -1,16 +1,12 @@
-class Command:
+class Point:
     def __init__(self):
-        self.cmd = {}
-
-    def com(self):
-        while True:
-            num = input('>>')
-            if num == 'quit':
-                break
-            if num in self.cmd:
-                print( self.cmd[num])
-            else:
-                self.cmd.get(0)
-
-
-Command().com()
+        print('init')
+    def __enter__(self):
+        print('enter')
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('exit')
+p = Point()
+with p as f:
+    print('in with -------')
+    print(p == f)
+    print('with over')
